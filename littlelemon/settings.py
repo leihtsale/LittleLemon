0d907ad0980fe39f54ddf75afe1202ae45ef64be
leihtsale <elasthiel@gmail.com>
2023-02-09 05:22:20 +0800

    Setup djoser and add djoser urls

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -135,3 +135,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER = {
+    "USER_ID_FIELD":"username",
+}
